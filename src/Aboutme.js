90d4ebabf9f6c95f5cd91750eab3254011422ef0
Thanura Rukshan <thanurarukshan2000@gmail.com>
2023-09-12 10:21:38 +0530

    my picture update

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -15,7 +15,7 @@ function Aboutme() {
 
   return (
     <div className="Aboutme" id="aboutMe">
-        <img src = "./me_web.png" className="Aboutme-me" data-aos="fade-up"/>
+        <img src = "./Thanurawithframe.png" className="Aboutme-me" data-aos="fade-up"/>
         <span className="Aboutme-aboutme" data-aos="fade-up" >Aboutme</span>
         <span className="Aboutme-dis" data-aos="fade-up">Hey there! I am an engineering undergraduate with an IT background. I am self-motivated and a fun, friendly person who is always willing to try new things. I work well in groups as well as on my own because I like to set goals for myself that I can achieve, and I have good listening and communication skills.</span>
         <a className="Aboutme-cv" data-aos="fade-up" onClick={handleDownloadCV}>Download CV</a>
